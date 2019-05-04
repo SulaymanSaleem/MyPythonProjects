55b60b1dbe8564290d7bbc1e45ff3dc64b538d7e
SulaymanSaleem <39617382+SulaymanSaleem@users.noreply.github.com>
2019-05-04 19:01:55 +0100

    Update Game.py
    Changed the order

--- AllMyGames/Game.py
+++ AllMyGames/Game.py
@@ -1,6 +1,8 @@
 import random
-print("You have 3 tries to guess a number between and including 1-9")
-print("Each time you run the script the number changes!")
+print("""
+You have 3 tries to guess a number between and including 1-9"
+Each time you run the script the number changes!
+""")
 
 secret_number = random.randint(1, 9)
 tries = 0
@@ -9,21 +11,19 @@ while tries < guess_limit:
 
     try:
         guess_number = int(input("Guess: "))
-
-        if guess_number < 1 or guess_number > 9:
+        if guess_number == secret_number:
+            msg = f'Well done! The number was {secret_number}'
+            print(msg)
+        elif guess_number < 1 or guess_number > 9:
             print("Enter a number between 1 and 9")
 
         elif guess_number != secret_number and tries == 2:
-            print("YOU LOSE")
+            print("YOU LOSE!!!")
             print("The number was ", secret_number)
             break
-        elif guess_number != secret_number:
-                print("Try again...")
-                tries = tries + 1
         else:
-            msg = f'Well done! The number was {secret_number}'
-            print(msg)
-            break
+            print("Try again...")
+            tries = tries + 1
 
     except ValueError:
         print("Enter a number")
